818f2b8fdebeb10a5519795f92109782aaeac887
Антон Сурыгин <surygin@live.com>
2021-08-02 15:01:42 +0300

    подредактировубрал лишнее

--- index.php
+++ index.php
@@ -3,18 +3,17 @@
 $route = $_SERVER['REQUEST_URI'];
 
 $routes = [
-  '/' . 'routing/'         =>  "/public/home.php",
+  '/' . 'routing/'     =>  "/public/home.php",
   "/routing/about"     =>  "/public/about.php",
   "/routing/contacts"  =>  "/public/contacts.php"
 ];
 
+var_dump($routes[$route]); print('<br>');
+var_dump($route);
+
 if(array_key_exists($route, $routes)){
-  include $routes[$route]; exit;
+  include($routes[$route]); exit;
 }
 else {
   print(404) . '<br>';
 }
-
-
-var_dump($routes[$route]); print('<br>');
-var_dump($route);
--- public/about.php
+++ public/about.php
@@ -1,4 +1,1 @@
 <h1>This is About page</h1>
-<?
-var_dump($routes[$route]); print('<br>');
-var_dump($route);
