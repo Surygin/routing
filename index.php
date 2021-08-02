edc66912ec3d0ab0bc07f5b1ae4e20da5ff5a13f
Антон Сурыгин <surygin@live.com>
2021-08-02 12:20:43 +0300

    Первый вариант роутинга

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$route = $_SERVER['REQUEST_URI'];
+
+$routes = [
+  '/' . 'routing/'         =>  "/public/home.php",
+  "/routing/about"     =>  "/public/about.php",
+  "/routing/contacts"  =>  "/public/contacts.php"
+];
+
+if(array_key_exists($route, $routes)){
+  include $routes[$route]; exit;
+}
+else {
+  print(404) . '<br>';
+}
+
+
+var_dump($routes[$route]); print('<br>');
+var_dump($route);
---
+++ public/home.php
@@ -0,0 +1,4 @@
+<h1>This is Home page</h1>
+<?
+var_dump($routes[$route]); print('<br>');
+var_dump($route);
